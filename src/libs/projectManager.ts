d5e8d955e8f2e1eeee7e0f7b9bb7e7c4d009ed79
akirco <earlfx@163.com>
2023-06-10 20:11:13 +0800

    complete 0.1.0

--- src/libs/projectManager.ts
+++ src/libs/projectManager.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
-import path = require("path");
+import * as path from "path";
 
 export function getProjectRootDir() {
   return vscode.workspace
